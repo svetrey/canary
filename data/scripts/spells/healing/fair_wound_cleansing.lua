dcbf46db356b89fc9c0c6da53116fc7ed26b08f6
svetrey <51045033+svetrey@users.noreply.github.com>
2025-05-06 22:07:41 +0100

    Update fair_wound_cleansing.lua
    Formula used previously was incorrectly doubling the effect of Level in this spell.
    Formula in Global Tibia is not just doubling the Wound Cleansing spell.
    Updated formula checked on few knights level 300-500 on Global Tibia and seem to be correct.
    https://tibia.fandom.com/wiki/Formulae

--- data/scripts/spells/healing/fair_wound_cleansing.lua
+++ data/scripts/spells/healing/fair_wound_cleansing.lua
@@ -4,9 +4,9 @@ combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_MAGIC_BLUE)
 combat:setParameter(COMBAT_PARAM_AGGRESSIVE, false)
 combat:setParameter(COMBAT_PARAM_DISPEL, CONDITION_PARALYZE)
 
-function onGetFormulaValues(_player, level, magicLevel) -- already compared to the official tibia | compared date: 05/07/19(m/d/y)
-	local min = (level * 0.2 + magicLevel * 4 + 25) * 2
-	local max = (level * 0.2 + magicLevel * 7.95 + 51) * 2
+function onGetFormulaValues(_player, level, magicLevel) -- already compared to the official tibia | compared date: 06/05/25(m/d/y)
+	local min = (level * 0.2 + magicLevel * 15.37) + 93
+	local max = (level * 0.2 + magicLevel * 21.04) + 134
 	return min, max
 end
 
